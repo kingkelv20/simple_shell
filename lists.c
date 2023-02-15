51885f3ae7c8c210ad4829f05bf9801363a9b394
kingkelv20 <kelvinagimogim@gmail.com>
2023-02-15 02:02:49 -0800

    simple_shell_group_projt

--- lists.c
+++ lists.c
@@ -1,12 +1,12 @@
 #include "shell.h"
 
 /**
- * add_node - adds a node to the start of the list
- * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
+ * add_node - adds a node to the start of the list.
+ * @head: address of pointer to head node.
+ * @str: str field of node.
+ * @num: node index used by history.
  *
- * Return: size of list
+ * Return: size of list.
  */
 list_t *add_node(list_t **head, const char *str, int num)
 {
@@ -34,10 +34,10 @@ list_t *add_node(list_t **head, const char *str, int num)
 }
 
 /**
- * add_node_end - adds a node to the end of the list
- * @head: address of pointer to head node
- * @str: str field of node
- * @num: node index used by history
+ * add_node_end - adds a node to the end of the list.
+ * @head: address of pointer to head node.
+ * @str: str field of node.
+ * @num: node index used by history.
  *
  * Return: size of list
  */
@@ -75,10 +75,10 @@ list_t *add_node_end(list_t **head, const char *str, int num)
 }
 
 /**
- * print_list_str - prints only the str element of a list_t linked list
- * @h: pointer to first node
+ * print_list_str - prints only the str element of a list_t linked list.
+ * @h: pointer to first node.
  *
- * Return: size of list
+ * Return: size of list.
  */
 size_t print_list_str(const list_t *h)
 {
@@ -96,8 +96,8 @@ size_t print_list_str(const list_t *h)
 
 /**
  * delete_node_at_index - deletes node at given index
- * @head: address of pointer to first node
- * @index: index of node to delete
+ * @head: address of pointer to first node.
+ * @index: index of node to delete.
  *
  * Return: 1 on success, 0 on failure
  */
